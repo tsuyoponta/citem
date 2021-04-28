1594c165346087d0ffe5404f71305e847c6429e9
tsuyoponta <tsuyo412ponta@gmail.com>
2021-04-28 13:18:56 +0900

    articlesテーブル作成

---
+++ db/migrate/20210428040050_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title,      null: false
+      t.string :text,       null: false
+      t.references :user,   foreign_key: true
+      t.timestamps
+    end
+  end
+end
