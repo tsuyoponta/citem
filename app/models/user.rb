aceea36eb31b98670e1da9d73800fc0f581f8083
tsuyoponta <tsuyo412ponta@gmail.com>
2021-05-03 14:56:19 +0900

    バリデーション nickname 文字数

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 10 }
   
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
