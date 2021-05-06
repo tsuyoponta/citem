adc3327ffec8ca5e72ff95c5ef7cc3c5f07fe80e
tsuyoponta <tsuyo412ponta@gmail.com>
2021-05-06 21:03:50 +0900

    ルーティング設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,14 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def new
+
+  end
+
+  def create
+    
+  end
+
   private
   def article_params
     params.require(:article).permit(:content, :image).merge(user_id: current_user.id)
