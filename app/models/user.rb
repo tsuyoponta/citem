4c38884be309c9121c75bbd34350e644974fe3a1
tsuyoponta <tsuyo412ponta@gmail.com>
2021-05-06 11:51:06 +0900

    アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
     
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で入力してください'
+
+  has_many :articles
 end
