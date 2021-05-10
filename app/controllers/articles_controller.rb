7be2984eb42af79c58e427bad8a770df89371c19
tsuyoponta <tsuyo412ponta@gmail.com>
2021-05-10 20:59:57 +0900

    indexアクション変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @articles = Article.includes(:user).order(id: 'DESC')
   end
 
   def new
