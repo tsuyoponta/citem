7f4e436aa10550bbefac15e85c3eb587ce2d03c9
tsuyoponta <tsuyo412ponta@gmail.com>
2021-06-03 09:35:20 +0900

    article_controller 空行追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     return unless @article.destroy
+    
     redirect_to root_path, warning: '記事を削除しました'
   end
 
@@ -51,6 +52,7 @@ class ArticlesController < ApplicationController
 
   def move_to_index
     return unless current_user.id != @article.user_id
+
     redirect_to root_path
   end
 end
